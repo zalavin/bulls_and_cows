c79e7fe0536cc7a46de8c5839880b52f6271f2d5
Yan Zalavin <yztrnc@gmail.com>
2019-01-02 19:16:57 +0200

    Implement first version

---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use std::io;
+use rand::Rng;
+
+fn main() {
+    println!("Welcome!");
+
+    // Generate a 4-digit number with different chars
+    let mut vec = Vec::new();
+    
+    while vec.len() < 4 {
+        let numb = rand::thread_rng().gen_range(0, 10);
+        
+        if !vec.contains(&numb) {
+            vec.push(numb);
+        }
+    }
+
+    let mut attempts = 1;
+    
+    loop {
+        // Get a number
+        let mut guess = String::new();
+    
+        io::stdin().read_line(&mut guess)
+             .expect("Failed to read line");
+            
+        let vec_str: String = vec.as_slice().into_iter().map(|i| i.to_string()).collect::<String>();
+        
+        // Return if number matches
+        if vec_str == guess.trim() {
+            println!("You have won with {} attempts!", attempts);
+            break;
+        }
+    
+        // Find bulls and cows
+        let mut bulls = 0;
+        let mut cows = 0;
+        let mut index = 0;
+        
+        // Split number into chars
+        let guess: Vec<u32> = guess
+            .trim()
+            .split("")
+            .filter(|s| !s.is_empty())
+            .map(|s| s.parse().unwrap())
+            .collect();
+        
+        for x in &guess {
+            if vec[index] == guess[index] {
+                bulls += 1;
+            } else if vec.contains(&x) {
+                cows += 1
+            }
+        
+            index += 1;
+        }
+    
+        // Display result
+        println!("{} bulls and {} cows", bulls, cows);
+    
+        // Increment attempts
+        attempts += 1;
+    }
+}
